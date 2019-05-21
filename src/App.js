927d24cd0afa78c020acfd4505ef29d70629232d
Harris Wang <harris.wang@mail.com>
2019-05-21 19:06:43 +1000

    update for tesla car and wheels

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header/Header.js';
-// import TeslaCar from './components/TeslaCar/TeslaCar.js';
-// import TeslaCounter from './components/TeslaCounter/TeslaCounter.js';
-// import TeslaClimate from './components/TeslaClimate/TeslaClimate.js';
-// import TeslaWheels from './components/TeslaWheels/TeslaWheels.js';
 import TeslaNotice from './components/TeslaNotice/TeslaNotice.js';
 import TeslaBattery from './containers/TeslaBattery.js';
 
--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import './TeslaBattery.css';
+import TeslaCar from '../components/TeslaCar/TeslaCar';
+// import TeslaCounter from './components/TeslaCounter/TeslaCounter.js';
+// import TeslaClimate from './components/TeslaClimate/TeslaClimate.js';
+// import TeslaWheels from './components/TeslaWheels/TeslaWheels.js';
+
 class TeslaBattery extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      carstats: [],
+      init: {
+        speed: 55,
+        temperature: 20,
+        climate: true,
+        wheels: 19
+      }
+    }
+  }
+  
   render() {
+    const { init } = this.state;
     return (
       <form className="tesla-battery">
         <h1>Range Per Charge</h1>
+        <TeslaCar wheelsize={ init.wheels } />
       </form>
     )
   }
