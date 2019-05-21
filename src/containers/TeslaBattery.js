cb3438ad05912c4013437739334e17f0fbf9a4f0
Harris Wang <harris.wang@mail.com>
2019-05-21 22:28:48 +1000

    Combine data into one file and display miles

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './TeslaBattery.css';
 import TeslaCar from '../components/TeslaCar/TeslaCar';
+import TeslaStats from '../components/TeslaStats/TeslaStats';
 // import TeslaCounter from './components/TeslaCounter/TeslaCounter.js';
 // import TeslaClimate from './components/TeslaClimate/TeslaClimate.js';
 // import TeslaWheels from './components/TeslaWheels/TeslaWheels.js';
-
+import battery from '../data/battery';
 class TeslaBattery extends React.Component {
   constructor(props) {
     super(props);
@@ -20,12 +21,37 @@ class TeslaBattery extends React.Component {
     }
   }
   
+  calculateStats = (models, value) => {
+    const dataModels  = battery;
+    return models.map(model => {
+      const { speed, temperature, climate, wheels } = value;
+      const miles = dataModels[model][wheels][climate ? 'on' : 'off'].speed[speed][temperature];
+      return {
+        model,
+        miles
+      };
+    })
+  }
+  
+    
+  statsUpdate() {
+    const carModels = ['60', '60D', '75', '75D', '90D', 'P100D'];
+    this.setState({
+      carstats: this.calculateStats(carModels, this.state.init)
+    })  
+  };
+    
+  componentDidMount() {
+    this.statsUpdate(); 
+  };
+
   render() {
-    const { init } = this.state;
+    const { init, carstats } = this.state;
     return (
       <form className="tesla-battery">
         <h1>Range Per Charge</h1>
         <TeslaCar wheelsize={ init.wheels } />
+        <TeslaStats carstats={ carstats } />
       </form>
     )
   }
