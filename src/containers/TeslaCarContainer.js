ab73f9c5b33a8cb1d678c96f3bf1218bc44a9430
Harris Wang <harris.wang@mail.com>
2019-09-12 03:14:36 +1000

    add redux packages and create tesla car container

---
+++ src/containers/TeslaCarContainer.js
@@ -0,0 +1,15 @@
+import TeslaCar from '../components/TeslaCar/TeslaCar';
+import { connect } from 'react-redux';
+
+const mapStateToProps = state => {
+	return {
+		wheelsize: state.config.wheels,
+	};
+};
+
+const TeslaCarContainer = connect(
+	mapStateToProps,
+	null
+)(TeslaCar);
+
+export default TeslaCarContainer;
