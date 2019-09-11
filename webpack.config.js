fc87b299d5f81562ccfaf44d73c1f4811edbbbbc
Harris Wang <harris.wang@mail.com>
2019-09-11 22:21:41 +1000

    add prod config for webpack and fix server bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const path = require("path");
 const webpack = require("webpack");
+const isProd = process.env.NODE_ENV === "production";
 
-module.exports = {
+const config = {
+  mode: !isProd ? "development" : "production",
   entry: "./src/index.js",
   resolve: {
     extensions: ["*", ".js", ".jsx"]
@@ -39,31 +44,31 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
         exclude: /node_modules/,
         use: [
-					{
-						loader: 'url-loader',
-						options: {
-							limit: 8192,
-							name: '[name].[ext]',
-							fallback: 'file-loader',
-							outputPath: 'images'
-						}
-					}
-				]
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+              name: "[name].[ext]",
+              fallback: "file-loader",
+              outputPath: "images"
+            }
+          }
+        ]
       },
       {
-				test: /\.(ttc|woff|woff2|eot|ttf)$/,
-				use: [
-					{
-						loader: 'url-loader',
-						options: {
-							limit: 8192,
-							name: '[name].[ext]',
-							fallback: 'file-loader',
-							outputPath: 'fonts'
-						}
-					}
-				]
-			},
+        test: /\.(ttc|woff|woff2|eot|ttf)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 8192,
+              name: "[name].[ext]",
+              fallback: "file-loader",
+              outputPath: "fonts"
+            }
+          }
+        ]
+      }
     ]
   },
   plugins: [
@@ -80,3 +85,48 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin()
   ]
 };
+
+if (isProd) {
+  Object.assign(config, {
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          test: /\.js(\?.*)?$/i,
+          parallel: true,
+          sourceMap: false
+        })
+      ]
+    }
+  });
+  config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
+  config.plugins.push(
+    new webpack.DefinePlugin({
+      "process.env": {
+        "NODE_ENV": JSON.stringify("production")
+      }
+    }),
+    new UglifyJSPlugin({
+      parallel: true,
+      ecma: 5,
+      minimize: isProd,
+      mangle: isProd,
+      output: {
+        comments: false
+      },
+      sourceMap: false,
+      compress: {
+        sequences: true,
+        dead_code: true,
+        conditionals: true,
+        booleans: true,
+        unused: true,
+        if_return: true,
+        join_vars: true,
+        loops: true,
+        passes: 3
+      }
+    })
+  );
+}
+
+module.exports = config;
